156f9153e09e0ab0f034944e9d96d321fbffc55b
Darthxbox <steve26@live.at>
2018-07-08 15:32:28 +0200

    initial commit

---
+++ DDL.me JDownloader Button.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         DDL.me JDownloader Button
+// @namespace    http://de.ddl.me/
+// @version      1.0
+// @description  try to take over the world!
+// @author       Sector
+// @include      *de.ddl.me/*
+// @grant        unsafeWindow
+// @run-at       document-idle
+// ==/UserScript==
+
+
+var htmlButton = "<b class='border'></b><a rel='3' alt='61600' class='' style='display: block;' onclick='sendToJD()'>JDownloader</a>";
+
+var links = [];
+var openload = "";
+var bar = unsafeWindow.document.getElementsByClassName("multibtn dlmode")[0].getElementsByTagName("div")[0];
+var submitForm = "<FORM id='jd' ACTION='http://127.0.0.1:9666/flash/add' target='hidden' METHOD='POST' hidden></FORM>"
+unsafeWindow.document.getElementById("adminmenu").innerHTML += submitForm;
+
+
+unsafeWindow.getDlLinks = function getDlLinks()
+{
+
+	var dropDown = unsafeWindow.document.getElementsByClassName("select_content")[0];
+	var parts = dropDown.getElementsByTagName("a");
+	for(var i = 0; i < parts.length; i++)
+	{
+		parts[i].click();
+		unsafeWindow.getOpenload();
+		dropDown = unsafeWindow.document.getElementsByClassName("select_content")[0];
+		parts = dropDown.getElementsByTagName("a");
+	}
+}
+unsafeWindow.getOpenload = function getOpenload(){
+	var arr = [];
+	var l = unsafeWindow.document.links;
+	for(var i=0; i<l.length; i++) {
+		if(l[i].href.includes('openload'))
+		{
+			openload += l[i].href+"\r\n";
+		}
+	}
+}
+
+unsafeWindow.getDlLinks();
+unsafeWindow.sendToJD = function sendToJD()
+{
+	var form = unsafeWindow.document.getElementById("jd");
+    form.innerHTML += "<INPUT TYPE='hidden' NAME='package' VALUE='TestName'>";
+	form.innerHTML += "<INPUT TYPE='hidden' NAME='urls' VALUE='" + openload + "'>";
+	form.submit();
+}
+
+
+bar.innerHTML += htmlButton;
